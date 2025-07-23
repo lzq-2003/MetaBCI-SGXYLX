2eab6a0b92af13e767a2462a2745dbeb5e7a3e7b
lzq-2003 <13485455673@163.com>
2025-07-23 19:17:39 +0800

    Add files via upload

---
+++ demos/demo_system/demo_TUNEXON_Online.py
@@ -0,0 +1,20 @@
+from metabci.brainflow.TUNEXON_OnlineMI import OnlineMI
+import threading
+
+if __name__ == '__main__':
+
+    M=OnlineMI(time_buffer=1, Stride=0.2, Name='ZSH',YuZhi=1.5,
+               MIwindow=6,MoShi='ZhuaWo',CiShu='Di 2 Ci')
+    M.device_connect()  # 创建设备连接
+
+    task1 = threading.Thread(target=M.data_read, name="data_read", daemon=True)  # 创建范式线程
+    task2 = threading.Thread(target=M.classify, name='classify', daemon=True)   # 创建数据采集线程
+    task3 = threading.Thread(target=M.serport, name='serport', daemon=True, args=('COM11','COM10'))  # 创建分类线程
+
+    task1.start()   # 启动范式线程
+    task2.start()   # 启动数据采集线程
+    task3.start()   # 启动分类线程
+
+    task1.join()
+    task2.join()
+    task3.join()
